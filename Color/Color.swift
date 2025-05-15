0e19d6d736d9b53938ac04555661f3349911cb02
rssxzp <rssxzp@icloud.com>
2025-05-15 20:51:53 +0500

    Inital commit, added launchscreen

---
+++ Color/Color.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension UIColor {
+    static var YPBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+    static var YPBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+    static var YPBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue }
+    static var YPGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var YPRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var YPWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+    static var YPWhiteAlpha: UIColor { UIColor(named: "YP WhiteAlpha") ?? UIColor.white}
+}
+
