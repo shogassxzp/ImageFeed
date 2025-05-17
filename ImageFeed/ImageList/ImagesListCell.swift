e6ec266d1ff4108c124215d0f1c97ae8f76b7483
rssxzp <rssxzp@icloud.com>
2025-05-17 21:53:58 +0500

    added fade effect with using CAGradientLayer

--- ImageFeed/ImageList/ImagesListCell.swift
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -4,5 +4,33 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet weak var tableLikeButton: UIButton!
     @IBOutlet weak var tableDataLabel: UILabel!
     @IBOutlet weak var tableImageView: UIImageView!
+    @IBOutlet weak var gradientView: UIView!
     static let reuseIdentifier = "ImagesListCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupGradient()
+    }
+        // MARK: Setup gradient
+        /*
+         Не догадался сделать в сториборде полупрозрачный вью по этому полез в жёсткие дебри и реализовал градиент кодом.
+         В уроке упоминалась вёрстка кодом в будущем, так что оставлю градиент в таком виде, надеюсь на ревьюер ругать за такое не будет
+         */
+    private func setupGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = gradientView.bounds
+        gradientLayer.colors = [
+            UIColor.black.withAlphaComponent(0.0).cgColor,
+            UIColor.black.withAlphaComponent(0.8).cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.5)
+        gradientView.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if let gradientLayer = gradientView.layer.sublayers?.first as? CAGradientLayer {
+            gradientLayer.frame = gradientView.bounds
+        }
+    }
 }
