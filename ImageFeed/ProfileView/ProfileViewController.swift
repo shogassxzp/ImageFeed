3d27579d4429c2af80794e18a9556683ca5a77b7
rssxzp <rssxzp@icloud.com>
2025-05-21 01:37:18 +0500

    fixed LogoutButton image

--- ImageFeed/ProfileView/ProfileViewController.swift
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -8,8 +8,14 @@
 import UIKit
 
 final class ProfileViewController:UIViewController {
+    @IBOutlet weak var LogoutButton: UIButton!
+    @IBOutlet weak var UserPhotoImageVIew: UIImageView!
     
+    @IBOutlet weak var BioLabel: UILabel!
+    @IBOutlet weak var UserNameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func LogoutPressed(_ sender: Any) {
     }
 }
