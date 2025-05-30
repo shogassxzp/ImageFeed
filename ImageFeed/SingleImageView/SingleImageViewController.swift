5e2632b44f33b554d1fc802a33c23e3347f110af
shogassxzp <rssxzp@icloud.com>
2025-05-31 02:44:45 +0500

    Переделан экран профиля, исправлен scrollView

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -10,84 +10,91 @@ final class SingleImageViewController: UIViewController {
             }
         }
     }
+    @IBOutlet weak var imageHeight: NSLayoutConstraint!
+    @IBOutlet weak var imageWidth: NSLayoutConstraint!
     
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet private var SingleImageView: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
         SingleImageView.image = image
+        // Изменение констант размера, чтобы всё считалось правильно
+        imageWidth.constant = SingleImageView.image?.size.width ?? 0
+        imageHeight.constant = SingleImageView.image?.size.height ?? 0
         scrollView.showsVerticalScrollIndicator = false
         scrollView.showsHorizontalScrollIndicator = false
         if let image = image {
             rescaleAndCenterImageInScrollView(image: image)
         }
     }
-    
+
     // MARK: Buttons actions
-    
+
     @IBAction func BackButtonTap(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
+
     @IBAction func ShareButtonTap(_ sender: Any) {
         guard let image = SingleImageView.image else {
             return
         }
         let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
-        
+
         present(activityController, animated: true, completion: nil)
     }
-    
+
     // MARK: Rescale and center
-    
+
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
-       
         view.layoutIfNeeded()
-        
-        //Размеры экрана и фотки
+
+        // Размеры экрана и фотки
         let visibleRectSize = scrollView.bounds.size
         let imageSize = image.size
-        
-        //Минимальный и максимальный масштаб
+
+        // Минимальный и максимальный масштаб
         let minZoomScale = scrollView.minimumZoomScale
         let maxZoomScale = scrollView.maximumZoomScale
         
-        //Вычисляем масштаб
+        // Вычисляем масштаб
         let hScale = visibleRectSize.width / imageSize.width
         let vScale = visibleRectSize.height / imageSize.height
         let scale = max(minZoomScale, min(maxZoomScale, max(hScale, vScale)))
-        
-        //Устанавливаем масштаб
-        scrollView.setZoomScale(scale, animated: false)
-        
-        //Убеждаемся, что layout обновился после изменения масштаба
-        scrollView.layoutIfNeeded()
-        
-        let newContenSize = scrollView.contentSize
 
-        let horizontalInset = min(0, (visibleRectSize.width - newContenSize.width) / 2)
-        let verticalInset = max(0, ((visibleRectSize.height - 0) - newContenSize.height) / 2)
-        //Устанавливаем изображение по середине
+        // Устанавливаем масштаб
+        scrollView.setZoomScale(scale, animated: false)
+
+        // Убеждаемся, что layout обновился после изменения масштаба
+        scrollView.layoutIfNeeded()
+
+        let newContentSize = scrollView.contentSize
+
+        let horizontalInset = max(0, (visibleRectSize.width - newContentSize.width) / 2)
+        let verticalInset = max(0, (visibleRectSize.height - newContentSize.height) / 2)
+        // Добавляем inset
         scrollView.contentInset = UIEdgeInsets(
             top: verticalInset,
             left: horizontalInset,
             bottom: verticalInset,
             right: horizontalInset
         )
+        
+        // Высчитываем центр
+        let xOffset = max(0, (newContentSize.width - visibleRectSize.width) / 2)
+        let yOffset = max(0, (newContentSize.height - visibleRectSize.height) / 2)
+        // Устанавливаем центр
+        scrollView.contentOffset = CGPoint(x: xOffset, y: yOffset)
+        
     }
-    
-    
 }
-    
-    
-    // MARK: Extensions
-    
-    extension SingleImageViewController: UIScrollViewDelegate {
-        func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-            return SingleImageView
-        }
-    }
 
+// MARK: Extensions
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return SingleImageView
+    }
+}
