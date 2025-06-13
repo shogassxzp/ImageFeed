a3e0cd3cfca4a0031e3ccd7af14e506ccbdb5bd2
shogassxzp <rssxzp@icloud.com>
2025-06-13 20:58:33 +0300

    BackButton configured

--- ImageFeed/AuthViewController.swift
+++ ImageFeed/AuthViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     
-
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
 
     @IBOutlet weak var loginButton: UIButton!
     
@@ -11,6 +11,14 @@ final class AuthViewController: UIViewController {
         
         loginButton.layer.masksToBounds = true
         loginButton.layer.cornerRadius = 16
-        
+        configureBackButton()
     }
+    
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "Backward")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "Backward")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: .none)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP Black")
+    }
+    
 }
--- ImageFeed/WebViewViewController.swift
+++ ImageFeed/WebViewViewController.swift
@@ -1,19 +1,14 @@
-//
-//  WebViewViewController.swift
-//  ImageFeed
-//
-//  Created by Игнат Рогачевич on 6/13/25.
-//
-
 import Foundation
 import WebKit
 
 final class WebViewViewController: UIViewController {
     
     @IBOutlet var webView: UIView!
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
     }
 }
