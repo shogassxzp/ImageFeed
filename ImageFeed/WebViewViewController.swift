d6aa820728d289f2ee0811353d1e225b3a50f861
shogassxzp <rssxzp@icloud.com>
2025-06-13 21:39:36 +0300

    added WebView request

--- ImageFeed/WebViewViewController.swift
+++ ImageFeed/WebViewViewController.swift
@@ -3,12 +3,30 @@ import WebKit
 
 final class WebViewViewController: UIViewController {
     
-    @IBOutlet var webView: UIView!
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    enum WebViewConstants {
         
+        static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString)!
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        
+        let url = urlComponents.url!
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
     }
 }
