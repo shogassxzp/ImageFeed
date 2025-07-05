4d6b405ca550fce77674c0bbe16f4216acb888f3
shogassxzp <rssxzp@icloud.com>
2025-07-05 08:03:53 +0500

    missed completion on main thread

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -6,5 +6,5 @@ enum Constants {
     static let secretKey = "lxaF30WLSCfDxFQHNeniQlPBozbduixJvXWBeyEB0BE"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")
 }
