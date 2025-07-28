a630e250ee49b7ee7d61dd441514e1d25877725c
shogassxzp <rssxzp@icloud.com>
2025-07-28 06:14:16 +0500

    feat: config ProgresHUD in AppDelegate

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -1,8 +1,12 @@
 import UIKit
+import ProgressHUD
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        ProgressHUD.animationType = .circleArcDotSpin
+        ProgressHUD.colorHUD = .white
+        ProgressHUD.colorAnimation = .black
         return true
     }
 
