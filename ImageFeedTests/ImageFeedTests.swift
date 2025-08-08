7cfccc84012e277d96304f96ae5d3fa4e3180ee2
shogassxzp <rssxzp@icloud.com>
2025-08-08 12:48:21 +0300

    feat: Созданы тесты для WebView
    Протестирован: вызовы viewDidLoad, loadRequest. скрытие progressBar при разных значениях, создание ссылки для аутентификации

---
+++ ImageFeedTests/ImageFeedTests.swift
@@ -0,0 +1,98 @@
+@testable import ImageFeed
+import XCTest
+
+final class ImageFeedTests: XCTestCase {
+    func testVievControllerCallsViewDidLoad() {
+        let viewController = WebViewViewController()
+        let presenter = WebViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+
+        _ = viewController.view
+
+        XCTAssertTrue(presenter.viewDidLoadCalled)
+    }
+
+    func testPresenterCallsLoadRequest() {
+       let viewController = WebViewControllerSpy()
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        presenter.viewDidLoad()
+        
+        XCTAssertTrue(viewController.loadRequestCalled)
+    }
+    
+    func testProgressVisibleWhenLessThenOne() {
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 0.6
+        
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        XCTAssertFalse(shouldHideProgress)
+    }
+    
+    func testProggresViewWhenEqualOne() {
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 1.0
+        
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        XCTAssertTrue(shouldHideProgress)
+    }
+    
+    func testAuthHelperAuthURL() {
+        let configuration = AuthConfiguration.standard
+        let authHelper = AuthHelper(configuration: .standard)
+        
+        let url = authHelper.authURL()
+        
+        guard let urlString = url?.absoluteString else {
+            XCTFail("Auth URL is nil")
+            return
+        }
+        XCTAssertTrue(urlString.contains(configuration.authStringURL))
+        XCTAssertTrue(urlString.contains(configuration.accessKey))
+        XCTAssertTrue(urlString.contains(configuration.accessScope))
+        XCTAssertTrue(urlString.contains(configuration.redirectURI))
+        XCTAssertTrue(urlString.contains("code"))
+    }
+}
+
+final class WebViewControllerSpy: WebViewViewControllerProtocol {
+    var presenter: WebViewPresenterProtocol?
+
+    var loadRequestCalled: Bool = false
+
+    func load(request: URLRequest) {
+        loadRequestCalled = true
+    }
+
+    func setProgressValue(_ newValue: Float) {
+    }
+
+    func setProgressHidden(_ isHidden: Bool) {
+    }
+}
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled: Bool = false
+    var loadRequestCalled: Bool = false
+
+    var view: WebViewViewControllerProtocol?
+
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+
+    func didUpdateProgressValue(_ newValue: Double) {
+    }
+
+    func code(from url: URL) -> String? {
+        return nil
+    }
+}
